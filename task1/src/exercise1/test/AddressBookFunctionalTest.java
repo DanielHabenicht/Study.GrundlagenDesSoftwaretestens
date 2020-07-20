603655d255761a9f6f42993efe487c38d0deef01
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-07-20 14:36:34 +0200

    initalize task 1

---
+++ task1/src/exercise1/test/AddressBookFunctionalTest.java
@@ -0,0 +1,64 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker, Theo Vassiliou and Diana Serbanescu
+ * Technische Universität Berlin
+ */
+package exercise1.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exercise1.addressbook.model.AddressBook;
+import exercise1.addressbook.model.EmailAddress;
+import exercise1.addressbook.model.Entry;
+import exercise1.addressbook.model.Gender;
+import exercise1.addressbook.model.PhoneNumber;
+import exercise1.addressbook.model.SizeLimitReachedException;
+
+/**
+ * Uebung 1 - Grundlagen des Testens mit JUnit
+ * 
+ * Bitte Nummer der Gruppe eintragen: 0
+ * 
+ * Bitte Gruppenmitglieder eintragen:
+ * 
+ * @author ...
+ * @author ...
+ * @author ...
+ * @author ...
+ */
+public class AddressBookFunctionalTest {
+
+	// The component under test
+	private AddressBook addressBook;
+
+	/*
+	 * Aufgabe 3a) Schreiben Sie eine Methode zum Aufsetzen der Testumgebung
+	 * ("Fixture"). Diese Methode soll automatisch vor jedem einzelnen JUnit
+	 * Testfall ausgeführt werden. Innerhalb der Methode soll mindestens ein
+	 * neues AddressBook Objekt angelegt und im Attribut "addressBook"
+	 * gepeichert werden.
+	 */
+
+
+	/*
+	 * Aufgabe 3b) Schreiben Sie einen JUnit Testfall zum überprüfen der
+	 * Funktionalität der addEntry() Methode.
+	 */
+
+
+	/*
+	 * Aufgabe 3c) Schreiben Sie einen JUnit Testfall zum überprüfen der
+	 * Funktionalität der getEntry() Methode.
+	 */
+
+	
+}
